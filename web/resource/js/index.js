eea4f7bc90657c125d5eedb20a927797f54a6b22
dapeng <usydapeng@163.com>
2014-12-04 10:36:18 +0800

    初始化项目

--- web/resource/js/index.js
+++ web/resource/js/index.js
@@ -3,9 +3,9 @@ $(function() {
 		speed: 700,               //  The speed to animate each slide (in milliseconds)
 		delay: 3000,              //  The delay between slide animations (in milliseconds)
 		//complete: function() {},  //  A function that gets called after every slide animation
+		fluid: true,              //  Support responsive design. May break non-responsive designs
 		keys: true,               //  Enable keyboard (left, right) arrow shortcuts
 		dots: true,               //  Display dot navigation
-		arrows: true,
-		fluid: true              //  Support responsive design. May break non-responsive designs
+		arrows: true
 	});
 });
